3347307f56ca350b918d862e2b1aafb7a073d530
PauloJose26 <paulojoliveira.2616@gmail.com>
2022-01-27 21:57:53 -0300

    style: Estilizaçao do component input e page login

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -9,7 +9,7 @@ import {
 } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 import { FieldError } from "react-hook-form";
-import { ForwardRefRenderFunction, forwardRef } from "react";
+import { ForwardRefRenderFunction, forwardRef, useState, useEffect } from "react";
 
 interface InputProps extends ChakraInputProps {
   label?: string;
@@ -17,28 +17,58 @@ interface InputProps extends ChakraInputProps {
   Icon?: IconType;
 }
 
+const BaseInput: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { label, error = null, Icon, ...rest },
+  ref
+) => {
+  const [inputBorder, setInputBorder] = useState("gray.200");
+  const [checkInput, setCheckInput] = useState<boolean>(false);
+  const [colorIcon, setColorIcon] = useState("gray.900");
 
+  useEffect(() => {
+    if (error) {
+      setInputBorder("red.500");
+      setColorIcon("red.500");
+    } else if(checkInput){
+      setInputBorder("green.500");
+      setColorIcon("green.500");
+    }
+  }, [error, checkInput]);
 
-const BaseInput: ForwardRefRenderFunction<
-  HTMLInputElement,
-  InputProps
-> = ({ label, error = null, Icon,  ...rest}, ref) => {
   return (
     <FormControl isInvalid={!!error}>
-      {label && (
-        <FormLabel>{ label }</FormLabel>
-      )}
+      {label && <FormLabel>{label}</FormLabel>}
       <InputGroup>
         {Icon && (
-            <InputLeftElement mt="7px">
-              <Icon />
-            </InputLeftElement>
-          )}
-        <ChakraInput h="55px" bg="gray.200" _focus={ {} } _hover={ {} } { ...rest } ref={ ref }/>
-        {error && (
-          <FormErrorMessage color={"red.700"}>{ error }</FormErrorMessage>
+          <InputLeftElement color={ colorIcon } mt="7px">
+            <Icon />
+          </InputLeftElement>
         )}
+        <ChakraInput
+          h="55px"
+          bg="gray.200"
+          border="2px solid"
+          color={ colorIcon }
+          borderColor={ inputBorder }
+          onChangeCapture={ (e) => {
+            if (e.currentTarget.value.length > 0) {
+              setCheckInput(true);
+            }
+            else {
+              setCheckInput(false);
+              setColorIcon("gray.900");
+              setInputBorder("gray.200")
+            }
+          }}
+          _focus={{}}
+          _hover={{}}
+          {...rest}
+          ref={ref}
+        />
       </InputGroup>
+      {error && (
+        <FormErrorMessage color={"red.700"}>{error.message}</FormErrorMessage>
+      )}
     </FormControl>
   );
 };
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -10,48 +10,116 @@ import {
 } from "@chakra-ui/react";
 import { Link as DomLink } from "react-router-dom";
 import { FaUserAlt, FaLock } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import Logo from "../../assets/Logo.svg";
 import { Input } from "../../components/Form/Input";
 
+interface FormLoginData {
+  email: string;
+  password: string;
+}
+
 export const Login = () => {
+  const schemaLogin = yup.object().shape({
+    email: yup
+      .string()
+      .required("* Campo obrigatório")
+      .email("* E-mail Inválido"),
+    password: yup.string().required("* Campo obrigatório"),
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormLoginData>({
+    resolver: yupResolver(schemaLogin),
+  });
+
+  const hundleLogin = (data: FormLoginData) => {
+    console.log(data);
+  };
+
   return (
     <Flex
       as="main"
       w="100%"
-      h="100vh"
+      overflowY="auto"
+      padding={["25px 5px", "25px 10px", "0"]}
+      h={["auto", "auto", "100vh"]}
+      flexDirection={["column", "column", "row"]}
       justifyContent="space-evenly"
       align="center"
-      bgGradient={"linear(to-r, blue.300 65%, yellow.300 35%)"}
+      bgGradient={[
+        "linear(to-t, blue.300 65%, yellow.300 35%)",
+        "linear(to-t, blue.300 65%, yellow.300 35%)",
+        "linear(to-r, blue.300 65%, yellow.300 35%)",
+      ]}
     >
-      <Flex align="stretch" w="30%">
-        <Image src={Logo} mr="30px" w="100px" />
-        <Box w="300px">
-          <Heading as="h1" fontSize="3xl" color="gray.900" mb="20px">
+      <Flex
+        align="stretch"
+        w={["80%", "70%", "30%"]}
+        mb={["25px", "25px", "0"]}
+        flexDirection={["column", "column", "row"]}
+        alignItems="center"
+      >
+        <Image
+          src={Logo}
+          mr={["0", "0", "30px"]}
+          mb={["25px", "25px", "0"]}
+          w={["45px", "45px", "100px"]}
+        />
+        <Box w={["200px", "200px", "300px"]}>
+          <Heading
+            as="h1"
+            fontSize={["xl", "2xl", "3xl"]}
+            color="gray.900"
+            mb="20px"
+          >
             MyPets
           </Heading>
-          <Text color="gray.900" fontSize="lg">
+          <Text color="gray.900" fontSize={["md", "md", "lg"]}>
             Uma maneira fácil e prática para gerenciar seus pets.
           </Text>
         </Box>
       </Flex>
       <Flex
         as="form"
-        w="40%"
+        onSubmit={ handleSubmit(hundleLogin) }
+        action="post"
+        w={["90%", "70%", "40%", "35%"]}
         bg="white"
         flexDirection="column"
         alignItems="center"
-        padding="40px"
+        padding={["20px", "30px", "25px 40px"]}
         borderRadius="10px"
       >
-        <Heading as="h1" textAlign="center" mb="20px">
+        <Heading as="h1" textAlign="center" mb="20px" fontWeight="normal">
           Login
         </Heading>
         <VStack w="100%" spacing="20px">
-          <Input Icon={ FaUserAlt } type="text" label="E-mail" placeholder="exemplo@exemplo.com" />
-          <Input Icon={ FaLock } type="password" label="Senha" placeholder="******" />
+          <Input
+            Icon={FaUserAlt}
+            type="text"
+            label="E-mail"
+            placeholder="exemplo@exemplo.com"
+            error={errors.email}
+            {...register("email")}
+          />
+          <Input
+            Icon={FaLock}
+            type="password"
+            label="Senha"
+            placeholder="******"
+            error={errors.password}
+            {...register("password")}
+          />
         </VStack>
         <Button
+          type="submit"
           bg="yellow.500"
           padding="25px"
           color="white"
@@ -66,7 +134,13 @@ export const Login = () => {
         </Button>
         <Text mt="30px" fontSize="lg">
           Ainda não possui conta?{" "}
-          <Link as={ DomLink } to="/register" fontSize="md" _hover={ {} } color="gray.400">
+          <Link
+            as={DomLink}
+            to="/register"
+            fontSize="md"
+            _hover={{}}
+            color="gray.400"
+          >
             Clique aqui
           </Link>{" "}
         </Text>
